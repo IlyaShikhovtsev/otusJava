6b1db7da52a2922e48df4c245de155dbef24b9be
i.shikhovtsev <shikhovtsev97@gmail.com>
2020-02-18 11:32:46 +0500

    Behavioral patterns. State

---
+++ L12-behavioral/src/main/java/ru/shikhovtsev/state/State.java
@@ -0,0 +1,15 @@
+package ru.shikhovtsev.state;
+
+public abstract class State {
+  Player player;
+
+
+  public State(Player player) {
+    this.player = player;
+  }
+
+  public abstract String onLock();
+  public abstract String onPlay();
+  public abstract String onNext();
+  public abstract String onPrevious();
+}
